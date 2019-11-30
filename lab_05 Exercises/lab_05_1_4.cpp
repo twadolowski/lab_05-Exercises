2ae32964bfd393c655f2b273b708f2dd1de10bb5
Tomasz Wądołowski <56031022+twadolowski@users.noreply.github.com>
2019-11-30 16:22:13 +0100

    Add files via upload

---
+++ lab_05 Exercises/lab_05_1_4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int x = 1, g = 2;
+
+int main(){
+					
+	int x = 2;		
+					
+	if (x > 1) {
+		x++;		
+		int x = 4;	
+		x++;		
+	}
+	std::cout << x;
+	std::cout << g;	
+
+	int x =-1;		
+					
+}
